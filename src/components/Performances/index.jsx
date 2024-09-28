59960e3dbcb0c889747af90af78c379db421e095
leollorca <leollorca@proton.me>
2024-09-28 15:37:03 +0200

    clean comments

--- src/components/Performances/index.jsx
+++ src/components/Performances/index.jsx
@@ -13,50 +13,6 @@ const Performances = ({ dataSource }) => {
 
   const getData = async () => {
     const data = await getPerformances(dataSource);
-    
-    // const formattedPerformances = data.data.map(performance => {
-    //   const subject = data.kind[performance.kind];
-    //   let order;
-    //   let formattedSubject;
-
-    //   switch (subject) {
-    //     case "intensity":
-    //       order = 1;
-    //       formattedSubject = "Intensité";
-    //       break;
-    //     case "speed":
-    //       order = 2;
-    //       formattedSubject = "Vitesse";
-    //       break;
-    //     case "strength":
-    //       order = 3;
-    //       formattedSubject = "Force";
-    //       break;
-    //     case "endurance":
-    //       order = 4;
-    //       formattedSubject = "Endurance";
-    //       break;
-    //     case "energy":
-    //       order = 5;
-    //       formattedSubject = "Energie";
-    //       break;
-    //     case "cardio":
-    //       order = 6;
-    //       formattedSubject = "Cardio";
-    //       break;
-    //     default:
-    //       formattedSubject = "Inconnu";
-    //       break;
-    //   }
-      
-    //   return {
-    //     order,
-    //     subject: formattedSubject,
-    //     value: performance.value,
-    //     fullMark: 300,
-    //   };
-    //   }
-    // );
 
     setPerformances(data);
   };
